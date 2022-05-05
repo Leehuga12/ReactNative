e9246a61fdd54a28fcdb115e6d7278fcaa4d716c
Leehuga12 <maddrenlb@gmail.com>
2022-05-05 18:01:20 +1200

    adding basic boiler template

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, Alert, Keyboard, TouchableWithoutFeedback } from 'react-native';
+
+
+
+export default function App() {
+
+
+  
+
+  return (
+    <View style={styles.container}>
+  </View>
+
+   );
+}
+
+
+
+const styles = StyleSheet.create({
+
+});
